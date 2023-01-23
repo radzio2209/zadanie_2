0aed1a2a69ed56e3ab074eb2e197b38bb067e3fc
radzio2209 <radoslaw.danielkiewicz@pollub.edu.pl>
2023-01-23 11:31:05 +0100

    aplikacja def

---
+++ client/src/OtherPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => {
+  return (
+    <div>
+      Multifib <br></br>
+      Radoslaw Danielkiewicz <br></br>
+      I2S 2.2 <br></br>
+      <Link to="/">Go back home</Link>
+    </div>
+  );
+};
